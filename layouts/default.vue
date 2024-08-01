ebc5543128b4e804c3f06fa481902ffa9e27ae5a
Igor Tkachenko <alone@ial.one>
2024-08-01 19:06:04 +0500

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style lang="scss">
+main {
+  padding: 16px;
+  display: block;
+  flex: 1;
+  flex-basis: auto;
+  overflow: auto;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      titleTemplate: '%s - Nuxt Template',
+      title: 'Test',
+    }
+  },
+  devtools: { enabled: true },
+  css: ['~/assets/main.scss'],
+
+  modules: [
+    '@pinia/nuxt',
+    'unplugin-icons/nuxt'
+  ],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  }
+})
